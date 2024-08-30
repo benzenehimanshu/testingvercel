fc18ec0c7a9b3ca186fb630eaf8b69c82ff03f16
Himanshu Kumar <benzenehimanshu@gmail.com>
2024-08-31 02:40:20 +0530

    first commit

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const port = process.env.PORT || 3000;
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./views"));
+app.set("views", path.join(__dirname, "views")); // Use "views" without "./"
 
 app.get("/", (req, res) => {
-  res.send("test");
+  res.render("index"); // Render the index.ejs file
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// module.exports = app;
+// module.exports = app; // Uncomment if you want to export the app for testing
