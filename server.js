8500fe4e46ec32e63930f6f37897eaa305c6cf40
Himanshu Kumar <benzenehimanshu@gmail.com>
2024-08-31 02:36:17 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "./views"));
 
 app.get("/", (req, res) => {
-  res.send("test.html");
+  res.send("test");
 });
 
 app.listen(port, () => {
